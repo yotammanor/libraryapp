6616492f052626d25be447714b67dca7f3c3b551
Yotam <yotammanor@gmail.com>
2015-11-07 15:59:53 +0200

    Reorder #1: REORDER in settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'zbb2a)v2e331z3m8%=z*!uum5-8s$#m3_!s+z4@+9fs$jaj9#)'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -98,8 +94,12 @@ USE_TZ = True
 
 LANGUAGE_BIDI = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ADMIN_REORDER = (
+    ("Auth", ("Users", "Groups")),
+    ("Items", ("Sites", "Images", "Maps", "Songs", "Travelouges")),
+)
